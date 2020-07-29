cfc69d15a82a516e2197a210cbe671b3c59c81ae
Glitch (activist-toolkit-kyla-and-winny) <none>
2020-07-29 23:01:51 +0000

    🏋🍩 Checkpoint ./take-action.html:19407098/58 ./script.js:19407098/92

--- script.js
+++ script.js
@@ -4,9 +4,10 @@
 
 /* TODO: Add a variable to store the "my-list" element */
 
+var myId=document.getElementById("myId");
 
 /* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
-
+myId.addEventListener("click", )
 
 /* TODO: Declare the function checkOffList and add actions inside the { } */
 
